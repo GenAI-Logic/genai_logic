5c6bcaf72632f016a278ef6194bf6939b8ed156c
Val Huber <valjhuber@gmail.com>
2025-03-14 12:47:51 -0700

    reset

--- webgenai/webg_projects/by-ulid/01JP6P6C0Z9D8E2YG97V3K4JGQ/docs/logic_suggestions/readme_logic_suggestions.md
+++
@@ -1,3 +0,0 @@
-### Ask GenAI for logic suggestions
-
-This is a work directory used by the system to capture [logic suggestions](https://apilogicserver.github.io/Docs/WebGenAI-CLI/#logic-suggestions).
--- webgenai/webg_projects/by-ulid/01JP732DJNM0EKQXAF2KA7VKKB/logic/wg_rules/Customer_Name_Constraint.py
+++
@@ -1,9 +0,0 @@
-
-from logic_bank.logic_bank import Rule
-from database.models import *
-import integration.kafka.kafka_producer as kafka_producer
-
-def init_rule():
-  Rule.constraint(validate=Customer,
-                  as_condition=lambda row: row.name != 'y',
-                  error_msg="Customer name cannot be 'y'")
